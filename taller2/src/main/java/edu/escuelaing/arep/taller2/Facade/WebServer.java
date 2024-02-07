310e770555ca4469d713284324d877893521a359
Santiago Forero Yate <santiago.forero-y@mail.escuelaing.edu.co>
2024-02-06 22:59:51 -0500

    retorno de la imagen completo, ajuste de estilos

--- taller2/src/main/java/edu/escuelaing/arep/taller2/Facade/WebServer.java
+++ taller2/src/main/java/edu/escuelaing/arep/taller2/Facade/WebServer.java
@@ -71,15 +71,14 @@ public class WebServer {
 
             System.out.println("ASi llego la peticion: " + petition);
 
-            outputLine = (petition.startsWith("/film")) ? movieInfo(petition.replace("/film?name=", ""))
-                    : mainPage(petition);
+            outputLine = (petition.startsWith("/film")) ? movieInfo(petition.replace("/film?name=", ""), clientSocket.getOutputStream())
+                    : mainPage(petition, clientSocket.getOutputStream());
 
             out.println(outputLine);
             out.close();
             in.close();
             clientSocket.close();
         }
-
         serverSocket.close();
     }
 
@@ -87,9 +86,10 @@ public class WebServer {
      * return a html structure with some movie information
      * 
      * @param name the name of the movie
+     * @param op OutputStream to return an image if is necessary
      * @return a html structure with some movie information, with some headers
      */
-    private static String movieInfo(String name) {
+    private static String movieInfo(String name, OutputStream op) {
         try {
 
             JsonObject resp = apf.searchMovie(name);
@@ -99,7 +99,7 @@ public class WebServer {
             String outputLine = "HTTP/1.1 200 OK\r\n"
                     + "Content-Type:text/html\r\n"
                     + "\r\n"
-                    + getStaticFile("/movieInfo.html").replace("{Title}", title.toString())
+                    + getStaticFile("/movieInfo.html", op).replace("{Title}", title.toString())
                             .replace("\"{Poster}\"", poster.toString()).replace("{Directors}", director.toString())
                             .replace("{Plot}", plot.toString());
 
@@ -116,18 +116,15 @@ public class WebServer {
      * 
      * @param filePetition the path where is allocate the html page and other files
      *                     that use this page
+     * @param op OutputStream to return an image if is necessary
      * @return the principal page of the application
      */
-    private static String mainPage(String filePetition) {
-
-        String mimeType = getMimeType(filePetition);
-        System.out.println("mimetype regresado para " + filePetition + ": " + mimeType);
-        String resp = getStaticFile(filePetition);
+    private static String mainPage(String filePetition, OutputStream op) {
 
         String outputLine = "HTTP/1.1 200 OK\r\n"
-                + "Content-Type:" + mimeType + "\r\n"
+                + "Content-Type:" + getMimeType(filePetition) + "\r\n"
                 + "\r\n"
-                + resp;
+                + getStaticFile(filePetition, op);
 
         return outputLine;
     }
@@ -141,33 +138,64 @@ public class WebServer {
     private static String getMimeType(String filePetition) {
         return (filePetition.endsWith(".html") || filePetition.endsWith("/")) ? "text/html"
                 : ((filePetition.endsWith(".css")) ? "text/css"
-                        : (filePetition.endsWith(".js")) ? "application/javascript" : (filePetition.endsWith(".jpg")) ? "image/jp2" : "text/plain");
+                        : (filePetition.endsWith(".js")) ? "application/javascript"
+                                : (filePetition.endsWith(".jpg")) ? "image/jpg" : "text/plain");
     }
 
     /**
      * this mehtod returns the static file related with the request
      * 
      * @param filePetition path of the file
+     * @param op OutputStream to return an image if is necessary
      * @return A string with all information insite the file
      */
-    private static String getStaticFile(String filePetition) {
+    private static String getStaticFile(String filePetition, OutputStream op) {
         Path file = (filePetition.equals("/")) ? Paths.get("target/classes/public/static/client.html")
-                : Paths.get("target/classes/public/static" + filePetition);
+                : (Paths.get("target/classes/public/static" + filePetition));
 
-        Charset charset = Charset.forName("UTF-8");
+        System.out.println(filePetition);
+        Charset charset = Charset.forName("ISO_8859_1");
         StringBuilder outputLine = new StringBuilder();
         try (BufferedReader reader = Files.newBufferedReader(file, charset)) {
             String line = null;
 
             while ((line = reader.readLine()) != null) {
+                if (filePetition.contains(".jpg")) {
+                    byte[] imageBytes = getAnImage(filePetition);
+                    String response = "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: image/jpg\r\n" +
+                            "Content-Length: " + imageBytes.length + "\r\n" +
+                            "\r\n";
+                            op.write(response.getBytes());
+                            op.write(imageBytes);
+                }
                 System.out.println(line);
                 outputLine.append(line).append("\n");
+
             }
         } catch (Exception e) {
-            System.err.format("IOException: "+ e.getMessage(), e);
+            // System.err.format("IOException: " + e.getMessage(), e);
+            e.printStackTrace();
         }
 
         return outputLine.toString();
+    }
+
+    /**
+     * return the bytes of an image 
+     * @param filePetition the route of the file to return to the browser
+     * @return an array of bytes
+     */
+    private static byte[] getAnImage(String filePetition) {
+
+        Path image = Paths.get("target/classes/public/static" + filePetition);
+
+        try {
+            return Files.readAllBytes(image);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
